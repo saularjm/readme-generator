18694d9613c364b01dc3d45264f9c2965e0bea53
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-05-31 16:39:03 -0700

    code runs, format looks good

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# readme-generator
+# README Generator ![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)
+
+## Table of Contents 
+
+- [Description](#description) 
+- [Installation](#Installation) 
+- [Usage](#Usage) 
+- [Tests](#Tests) 
+- [License](#License) 
+- [Contributors](#Contributors) 
+- [Questions](#Questions)
+            
+# Description 
+
+- This project creates a README.md file based on info collected from the command line 
+
+# Installation 
+
+- Run index.js with node and follow prompts
+
+# Usage
+
+- Creating streamlined READMEs
+
+# License
+            
+- None 
+            
+# Contributors 
+            
+- Saular Moaddeli and https://naereen.github.io/badges/ 
+            
+# Tests 
+            
+- Debugging in VSCode 
+            
+# Questions 
+            
+- Email: null 
+            
+![profile image](https://avatars1.githubusercontent.com/u/60551953?v=4)
--- index.js
+++ index.js
@@ -1,103 +1,109 @@
-// Require npm packages and assign to consts
+// Require node packages
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const util = require("util");
 
-// Promisify fs.writeFile
-const writeFileAsync = util.promisify(fs.writeFile);
-
-// Function to prompt user in CLI for info to populate README
-function promptUser() {
-  
-    // Get GitHub info from user
-    return inquirer.prompt([
-        {
-            message: "Enter your GitHub username:",
-            name: "username",
-        },
-        {
-            message: "What is the title of your project?",
-            name: "title"
-        },
-        {
-            message: "What is a brief description of your project?",
-            name: "description"
-        },
-        {
-            message: "How is your project installed?",
-            name: "installation"
-        },
-        {
-            message: "What will this project be used for?",
-            name: "usage"
-        },
-        {
-            message: "Who contributed to this project?",
-            name: "contributors"
-        },
-        {
-            message: "What tests were done on this project?",
-            name: "tests"
-        },
-        {
-            message: "Which license would you like to use?",
-            name: "license"
-        }
-    ]);
-}
-
-// Function to create README format
-async function generateMarkdown(res) {
-
-    // Create URL for GitHub API call
-    const queryUrl = `https://api.github.com/users/${res.username}`;
-
-    // Use GitHub API to retrieve user info
-    await axios.get(queryUrl).then(function(userInfo) {
-    email = userInfo.data.email;
-    picture = userInfo.data.avatar_url;
+// Prompt user
+inquirer
+    // Get Github username first for Axios call
+    .prompt({
+        message: "Enter your GitHub username:",
+        name: "username",
     })
 
-    return `
-        # ${res.title} [![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)](https://GitHub.com/Naereen/)
-        \n
-        ## Table of Contents \n
-        - [Description](#description) \n
-        - [Installation](#Installation) \n
-        - [Usage](#Usage) \n
-        - [Tests](#Tests) \n
-        - [License](#License) \n
-        - [Contributors](#Contributors) \n
-        - [Questions](#Questions) \n
-        ## Description \n
-        - ${res.description} \n
-        ## Installation \n
-        - ${res.installation} \n
-        ## Usage \n
-        - ${res.usage} \n
-        ## Tests \n
-        - ${res.tests} \n
-        ### License
-        - ${res.license} \n
-        ### Contributors \n
-        - ${res.contributors} \n
-        ### Questions \n
-        - ${email} \n
-        ![profile pic](${picture})`;
-}
+    .then(function ({ username }) {
 
-// Call functions
-promptUser()
-    .then(function(answers) {
-        const MD = generateMarkdown(answers);
+        // Build URL for Github API
+        const queryUrl = `https://api.github.com/users/${username}`;
 
-        // Write README file using data retrieved from CLI
-        return writeFileAsync("README.md", MD);
-    })
-    .then(function() {
-        console.log("Success!");
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
+        // API call
+        axios.get(queryUrl).then(function(userInfo){
+        email = userInfo.data.email;
+        picture = userInfo.data.avatar_url;
+
+        // Prompt user for README info
+        inquirer
+            .prompt ([
+            {
+                message: "What is the title of your project?",
+                name: "title"
+            },
+            {
+                message: "What is a brief description of your project?",
+                name: "description"
+            },
+            {
+                message: "How is your project installed?",
+                name: "installation"
+            },
+            {
+                message: "What will this project be used for?",
+                name: "usage"
+            },
+            {
+                message: "Who contributed to this project?",
+                name: "contributors"
+            },
+            {
+                message: "What tests were done on this project?",
+                name: "tests"
+            },
+            {
+                message: "Which license would you like to use?",
+                name: "license"
+            }
+            ]).then(function(res) {
+                //Build README format
+                const answers =
+                `# ${res.title} ![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)
+
+## Table of Contents 
+
+- [Description](#description) 
+- [Installation](#Installation) 
+- [Usage](#Usage) 
+- [Tests](#Tests) 
+- [License](#License) 
+- [Contributors](#Contributors) 
+- [Questions](#Questions)
+            
+# Description 
+
+- ${res.description} 
+
+# Installation 
+
+- ${res.installation}
+
+# Usage
+
+- ${res.usage}
+
+# License
+            
+- ${res.license} 
+            
+# Contributors 
+            
+- ${res.contributors} 
+            
+# Tests 
+            
+- ${res.tests} 
+            
+# Questions 
+            
+- Email: ${email} 
+            
+![profile image](${picture})`;
+          
+            //Write README file
+            fs.writeFile("README.md", answers, function(err) {
+                if (err) {
+                    return console.log(err);
+                } 
+                console.log("Success!")
+            })
+        })
+      })
+})
